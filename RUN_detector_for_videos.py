bdc8e8073717c75287c6180063a8fa7f1c8aa8e2
Integrationasd <104488266+Integrationasd@users.noreply.github.com>
2022-11-05 20:06:49 +0800

    Update RUN_detector_for_videos.py

--- RUN_detector_for_videos.py
+++ RUN_detector_for_videos.py
@@ -5,15 +5,17 @@
 # to avoid potential errors in real time       #
 ################################################
 
+from itertools import product
 import cv2
+from numpy import round
 from object_detection import ObjectDetection
 
 od = ObjectDetection()
 
 # video path, edit the input path here
 input_path='test.mp4'
-# output path, edit the output path here
-output_path='PNGoutput'
+# output path
+output_path='PNGresults'
 
 #load videos
 cap = cv2.VideoCapture(input_path)
@@ -21,41 +23,37 @@ cap = cv2.VideoCapture(input_path)
 number=0 #this counts of the number of frames, is used at naming outputs 
 while True:
     # capture a picture form video
-    _, frame = cap.read()
-
+    ret, frame = cap.read()
+    
     # run the network, get the outputs 
     (class_ids, scores, boxes) = od.detect(frame)
+    scores=round(scores,2)
 
-    #open classes name file
-    file=open("dnn_model/classes.txt")
-    classes=file.readlines()
-    file.close()
+    #Print the boxes n ids
+    for box, ids in product(boxes, class_ids):
+         #1.get datas to put
+        (x, y, w, h) = box
+        text='code: %s' %(str(ids))
 
-    #Print the boxes
-    for box in boxes:
-        for id in class_ids:
-
-            #1.get datas to put
-            (x, y, w, h) = box
-            text=classes[id]
-
-            #2.plot them on figure
-            cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
-            cv2.putText(frame,text,(x, y),cv2.FONT_HERSHEY_SIMPLEX,0.5,(0,255,0),2)
-
+        #2.plot them on figure
+        cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+        cv2.putText(frame,text,(x, y),cv2.FONT_HERSHEY_SIMPLEX,0.5,(0,255,0),2)
+            
     #save images
     number=number+1
-    savePath=output_path+'%s.jpg' %(str(number))
+    savePath='%s\%s.jpg' %(output_path, str(number))
+
     cv2.imwrite(savePath,frame)
-    print('figure %s saved' %(str(number)))
+    print('figure No. %s saved' %(str(number)))
             
     #show output
-    cv2.imshow("dnn_model/output", frame)
+    cv2.imshow("output", frame)
 
     #end loop,press esc to exit
     k=cv2.waitKey(1)
     if k==27:
         break
+    
 
 #end program
 cap.release()
